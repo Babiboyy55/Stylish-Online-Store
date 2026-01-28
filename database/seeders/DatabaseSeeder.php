b2a16fc9c32a4ecb43447de2ee2c699925d0ddc1
dodorafust <phanphatdz@gmail.com>
2026-01-28 21:55:10 +0700

    data

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::updateOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        // Create admin user (credentials from .env or defaults)
+        $this->call(\Database\Seeders\AdminUserSeeder::class);
+        $this->call(\Database\Seeders\ProductsTableSeeder::class);
+    }
+}
